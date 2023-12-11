c94741331a292744ccc5b4f7b618cc64edcf22d2
Campbell McDiarmid <campbell.mcdiarmid@icloud.com>
2023-12-11 21:43:55 +1300

    Solved D3P2, swap between P1 and P2 with #define PART_2

--- src/day3.c
+++ src/day3.c
@@ -7,9 +7,10 @@
 
 #define LINEWIDTH 256
 #define LINES 3
-// #define DEBUG
+#define PART_2
 
-int line_pn_sum(
+
+int line_sum(
     const char buffer[LINES][LINEWIDTH]
 )
 {
@@ -33,47 +34,161 @@ int line_pn_sum(
         exit(1);
     }
 
-    int nptr_ofs = 0, sptr_ofs = 0;
     unsigned long sum = 0, value = 0;
+    int start_idx = 0, search_offset = 0;
+#ifndef PART_2
 
-#ifdef DEBUG
-    printf("BUFFER:\n");
-    for (int rel_idx = 0; rel_idx < LINES; rel_idx++)
+    while (regexec(&num_reg, &buffer[1][start_idx], 1, &num_match, 0) == 0)
     {
-        if (!rel_idx)
-        {
-            printf("  ");
-            for (int z=0; z<strlen(buffer[1]); z++)
-                printf("%d",z%10);
-            printf("\n");
-        }
-        printf("%d %s\n", rel_idx, buffer[rel_idx]);
-    }
-    printf("\n");
-#endif
+        value = atoi(&buffer[1][start_idx + num_match.rm_so]);
+        search_offset = start_idx + num_match.rm_so - (int)(start_idx + num_match.rm_so > 0);
 
-    while (regexec(&num_reg, &buffer[1][nptr_ofs], 1, &num_match, 0) == 0)
-    {
-        value = atoi(&buffer[1][nptr_ofs + num_match.rm_so]);
-        sptr_ofs = nptr_ofs + num_match.rm_so - (int)(nptr_ofs + num_match.rm_so > 0);
-
-        for (int rel_idx = 0; rel_idx < LINES; rel_idx++)
+        for (int row = 0; row < LINES; row++)
         {
-            int search_result = regexec(&sym_reg, &buffer[rel_idx][sptr_ofs], 1, &sym_match, 0);
+            int search_result = regexec(&sym_reg, &buffer[row][search_offset], 1, &sym_match, 0);
             if (search_result == 0)
             {
-                if ((sptr_ofs + sym_match.rm_so) <= (nptr_ofs + num_match.rm_eo))
+                if ((search_offset + sym_match.rm_so) <= (start_idx + num_match.rm_eo))
                 {
                     sum += value;
                     break;
                 }
             }
         }
-        nptr_ofs += num_match.rm_eo;
+        start_idx += num_match.rm_eo;
+    }
+#else
+    size_t lw = strlen(buffer[1]);
+
+    // This time we're searching for symbols on outer loop
+    while (regexec(&sym_reg, &buffer[1][start_idx], 1, &sym_match, 0) == 0)
+    {
+        start_idx += sym_match.rm_so;
+
+        int touching_pns = 0;
+        int touching[2] = {0};
+
+        for (int row = 0; row < LINES; row++)
+        {
+            search_offset = 0;
+            while (regexec(&num_reg, &buffer[row][search_offset], 1, &num_match, 0) == 0)
+            {
+                value = atoi(&buffer[row][search_offset + num_match.rm_so]);
+
+                int lower_bound = start_idx - (int) (start_idx > 0);
+                int upper_bound = start_idx + (int) (start_idx != (lw-1));
+                if (
+                    ((search_offset + num_match.rm_so) <= upper_bound) && 
+                    ((search_offset + num_match.rm_eo - 1) >= lower_bound)
+                )
+                    touching[(++touching_pns)%2] = value;
+
+                search_offset += num_match.rm_eo;
+            }
+        }
+
+        if (touching_pns == 2)
+            sum += touching[0] * touching[1];
+
+        start_idx++;
+    }
+#endif
+    regfree(&num_reg);
+    regfree(&sym_reg);
+
+    return sum;
+}
+
+
+int line_gr_sum(
+    const char buffer[LINES][LINEWIDTH]
+)
+{
+    /*
+    * 1. Find numbers in curr
+    * 2. Check for touching symbols in prev, curr, next
+    * 3. If check is true, add number to sum
+    * 4. Return sum
+    */
+    regex_t num_reg;
+    regex_t sym_reg;
+    regmatch_t num_match;
+    regmatch_t sym_match;
+
+    if (
+        regcomp(&num_reg, "([0-9]+)", REG_EXTENDED) |
+        regcomp(&sym_reg, "([*])", REG_EXTENDED)
+    )
+    {
+        fprintf(stderr, "Failed to compile regex\n");
+        exit(1);
+    }
+
+    int sym_idx = 0, search_offset = 0;
+    unsigned long sum = 0, value = 0;
+    size_t lw = strlen(buffer[1]);
+
+#ifdef DEBUG
+    printf("BUFFER:\n");
+    for (int row = 0; row < LINES; row++)
+    {
+        if (!row)
+        {
+            printf("  ");
+            for (int z=0; z<strlen(buffer[1]); z++)
+                printf("%d",z%10);
+            printf("\n");
+        }
+        printf("%d %s\n", row, buffer[row]);
+    }
+    printf("\n");
+#endif
+    // This time we're searching for symbols on outer loop
+    while (regexec(&sym_reg, &buffer[1][sym_idx], 1, &sym_match, 0) == 0)
+    {
+        sym_idx += sym_match.rm_so;
+        // printf("Gear in column %d\n", sym_idx);
+
+        int touching_pns = 0;
+        int touching[2] = {0};
+
+        for (int row = 0; row < LINES; row++)
+        {
+            search_offset = 0;
+            while (regexec(&num_reg, &buffer[row][search_offset], 1, &num_match, 0) == 0)
+            {
+                value = atoi(&buffer[row][search_offset + num_match.rm_so]);
+                // printf("Value %lu at (%d, %lld) is ", value, row, search_offset + num_match.rm_so);
+
+                int lower_bound = sym_idx - (int) (sym_idx > 0);
+                int upper_bound = sym_idx + (int) (sym_idx != (lw-1));
+                if (
+                    ((search_offset + num_match.rm_so) <= upper_bound) && 
+                    ((search_offset + num_match.rm_eo - 1) >= lower_bound)
+                )
+                {
+                    // printf("touching!\n");
+                    touching[(++touching_pns)%2] = value;
+                }
+                else
+                {
+                    // printf("NOT!\n");
+                }
+                search_offset += num_match.rm_eo;
+            }
+        }
+        if (touching_pns == 2)
+        {
+            // printf("GR %lu -> %lu\n", sum, sum + (touching[0] * touching[1]));
+            sum += touching[0] * touching[1];
+        }
+
+        sym_idx++;
     }
 
     regfree(&num_reg);
     regfree(&sym_reg);
+    // printf("\n");
 
     return sum;
 }
@@ -102,7 +217,7 @@ int main(int argc, char* argv[])
     // Iterate through input file lines
     while (fgets(buffer[LINES-1], sizeof(buffer[LINES-1]), fileptr) || *buffer[1]) {
         buffer[LINES-1][strcspn(buffer[LINES-1], "\n")] = 0;
-        tally += line_pn_sum(buffer);
+        tally += line_sum(buffer);
 
         // Roll lines
         memset(buffer[0], 0, sizeof(char) * LINEWIDTH);
